890c1b6f4f4c5b7b671b49bd831be82bca91eaf5
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-05-09 21:47:41 +0530

    add feature for sending mail

--- src/main/java/com/flightreservation/flightreservation/util/EmailUtil.java
+++ src/main/java/com/flightreservation/flightreservation/util/EmailUtil.java
@@ -1,5 +1,5 @@
 package com.flightreservation.flightreservation.util;
-
+import java.io.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
